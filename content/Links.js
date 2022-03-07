684ca77b0d64d411855045e98241f529f4fbd735
Tej <tkahlon@labx.com>
2022-03-06 19:02:35 -0500

    v2

--- content/Links.js
+++ content/Links.js
@@ -1,9 +1,18 @@
 import React from "react";
-import { FaChild } from "react-icons/fa";
+import { FaChild, FaTwitter } from "react-icons/fa";
 
 const Links = () => {
   return (
     <>
+      <a
+        href="https://www.twitter.com/jackalislive"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Follow Jackal"
+        title="Follow Jackal"
+      >
+        <FaTwitter />
+      </a>
       <a
         className="children"
         href="https://www.savethechildren.org/us/ways-to-help"
