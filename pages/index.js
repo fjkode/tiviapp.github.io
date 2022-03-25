6d480e7ad70bde6106b14b55364bf4bc8c9148d2
Tej <tkahlon@labx.com>
2022-03-25 00:39:52 -0400

    v3

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,15 @@
 import Head from "next/head";
-import React, { useEffect } from "react";
+import React from "react";
 import { Slide, ToastContainer } from "react-toastify";
 import { getData } from "../common";
-import { darkNotificationBottom } from "../common/notification";
 import App from "../components/App";
 import Error from "../content/Error";
 
 export default function Home({ listing }) {
-  // useEffect(
-  //   () =>
-  //     darkNotificationBottom(
-  //       "Always use HTTP protocol when viewing this site for seamless experience. Check FAQs section for more information."
-  //     ),
-  //   []
-  // );
-  // setTimeout(
-  //   () =>
-  //     darkNotificationBottom(
-  //       "If you can, please make a difference in a child's life by donating books, clothes etc. or any help in whatever way possible for you."
-  //     ),
-  //   60000
-  // );
   return (
     <>
       <Head>
         <meta name="title" content="Jackal" />
-        <meta name="description" content="Watch TV beyond boundaries." />
-        <meta
-          name="keywords"
-          content="tv, live-tv, open-source, internet, video, entertainment, jackal"
-        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="robots" content="index, follow" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
