8e535a858022af078af07917633205d34809b90e
Tej <tkahlon@labx.com>
2022-03-28 12:41:02 -0400

    v3

--- pages/_document.js
+++ pages/_document.js
@@ -33,6 +33,11 @@ class MyDocument extends Document {
           <link rel="shortcut icon" type="image/png" href="./logo.png" />
           <link rel="icon" type="image/png" href="./logo.png" />
           <link rel="apple-touch-icon" type="image/png" href="./logo.png" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"tpkahlon",utcoffset:"-5"}))};sessionStorage.setItem("_swa","1");`,
+            }}
+          ></script>
         </Head>
         <body>
           <Main />
